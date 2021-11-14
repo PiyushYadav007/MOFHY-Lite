65d8781e1e470b041aa0dcea2e68ea77c64f260e
WhiteHacker993 <93871548+WhiteHacker993@users.noreply.github.com>
2021-11-14 08:49:58 +0530

    Update viewaccount.php

--- src/viewaccount.php
+++ src/viewaccount.php
@@ -24,7 +24,7 @@ else{
 	$PageInfo = ['title'=>'Unathorized Access','rel'=>''];
 	require_once __DIR__.'/includes/Connect.php';
 	require_once __DIR__.'/handler/AreaHandler.php';
-	require_once __DIR__.'/includes/Header.php';
+	require_once __DIR__.'/includes/Header2.php';
 	require_once __DIR__.'/handler/CookieHandler.php';
 	require_once __DIR__.'/includes/Navbar.php';
 	require_once __DIR__.'/includes/Sidebar.php';
